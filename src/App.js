0f9c60d84c8f1a8c0fb2ded82722e399554ca547
Vishaveet <119399234+Vishaveet@users.noreply.github.com>
2023-09-06 18:30:57 +0530

    add some functions

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./app.css";
 import AddContact from "./Addcontact";
-import EditContact from "./EditContact";
 import Display from "./Display";
-import DeleteContact from "./DeleteContact";
 
 function App() {
   const [data, setData] = useState([]);
   const [isture, setIsture] = useState();
-  const [isHome, setIsHome] = useState(false);
   useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/users")
@@ -53,7 +50,7 @@ function App() {
           </div>
         </div>
         <div className={isture ? "new-box" : ""}>
-          {isture ? <AddContact home={isHome} bol={handleChange} /> : ""}
+          {isture ? <AddContact  bol={handleChange} /> : ""}
         </div>
       </div>
     </>
@@ -61,15 +58,3 @@ function App() {
 }
 
 export default App;
-
-{
-  /* <div className="conatct-box">
-              <img
-                alt="img"
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrY_W6KrP-a7AEgV1EGganXGC4LBZ44E4d3WwyoeA-sw&s"
-              />
-              <h3>Raj </h3>
-            </div> */
-}
-
-// {(!isture && isEdit) ? <EditContact item={item.name}/>: ""}
--- src/Display.js
+++ src/Display.js
@@ -1,55 +1,74 @@
+import axios from "axios";
 import { useState } from "react";
 
 function Display(props) {
+
   let detail = props.name;
-  console.log(props);
-  const [data, setData] = useState(detail);
   const [isbol, setIsbol] = useState(true);
+  const [name, setName] = useState(props.name);
   const handleChange = () => {
     setIsbol((prev) => (prev = !prev));
   };
+
   const handleEdit = () => {
-    console.log("edit");
-    setData("Edit");
+    axios
+      .put(`https://jsonplaceholder.typicode.com/users`)
+      .then((response) => {
+        // console.log("res", response.data);
+        // setData(response.data);
+      })
+      .catch((error) => {
+        // console.log("error");
+      });
+    let newname=prompt("Enter a new name",name);
+    setName(newname);
   };
+
   const handleDelete = () => {
-    setData("Del");
-    alert('delete')
-    console.log("delete");
+   alert(`Delete ${props.name}`);
+   axios
+      .delete("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        // console.log("res", response.data);
+      })
+      .catch((error) => {
+        // console.log("error");
+      });
+
   };
   return (
     <>
-      <div className="conatct-box" onClick={handleChange}>
-        {/* {data} */}
-        <img
-          className="profile"
-          alt="img"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrY_W6KrP-a7AEgV1EGganXGC4LBZ44E4d3WwyoeA-sw&s"
-        />
+      
+        <div className="conatct-box" onClick={handleChange}>
+          <img
+            className="profile"
+            alt="img"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrY_W6KrP-a7AEgV1EGganXGC4LBZ44E4d3WwyoeA-sw&s"
+          />
 
-        <h3>{props.name}</h3>
-        {isbol ? (
-          ""
-        ) : (
-          <img
-            onClick={handleEdit}
-            className="edit"
-            alt="img"
-            // onClick={}
-            src="https://cdn-icons-png.flaticon.com/128/2356/2356780.png"
-          />
-        )}
-        {isbol ? (
-          ""
-        ) : (
-          <img
-            onClick={handleDelete}
-            className="delete"
-            alt="img"
-            src="https://cdn-icons-png.flaticon.com/128/3096/3096687.png"
-          />
-        )}
-      </div>
+          <h3>{name}</h3>
+          {isbol ? (
+            ""
+          ) : (
+            <img
+              onClick={handleEdit}
+              className="edit"
+              alt="img"
+              src="https://cdn-icons-png.flaticon.com/128/2356/2356780.png"
+            />
+          )}
+          {isbol ? (
+            ""
+          ) : (
+            <img
+              onClick={handleDelete}
+              className="delete"
+              alt="img"
+              src="https://cdn-icons-png.flaticon.com/128/3096/3096687.png"
+            />
+          )}
+        </div>
+       
     </>
   );
 }
